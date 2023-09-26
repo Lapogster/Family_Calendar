4c7783752b02c08cc3b4465ea20f97fdc7b2d6ef
Lapogster <lpograri@gmail.com>
2023-09-26 20:56:14 +1000

    Initial Project Creation
    Made the xcode project

---
+++ Family Calendar/Family Calendar/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Family Calendar
+//
+//  Created by Luca Pograri on 26/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+        }
+    }
+}
---
+++ Family Calendar/Family Calendar/Family_CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Family_CalendarApp.swift
+//  Family Calendar
+//
+//  Created by Luca Pograri on 26/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Family_CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
